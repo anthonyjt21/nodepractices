f0c50605724f8f3ee2608f52bdac3abc9e7e1a84
ajimenez <anthonyjt21>
2021-02-20 17:10:33 -0600

    configuracion de routas node

--- app.js
+++ app.js
@@ -1,49 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-/*function rqListener(){
-
-}*/
-//function (req, res) {}
-const server = http.createServer((req, res) => {
-	//console.log(req.url, req.method, req.headers);
-	//	process.exit();
-	const url = req.url;
-	const method = req.method;
-	if (url === '/') {
-		res.setHeader('Content-Type', 'text/html');
-		res.write('<html>');
-		res.write('<head><title>Enter message</title></head>');
-		res.write(
-			'<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Submit</button></form></body>',
-		);
-		res.write('</html>');
-		return res.end();
-	}
-
-	if (url === '/message' && method === 'POST') {
-		fs.writeFileSync('message.txt', 'DUMMY');
-		//res.writeHead(302,{});
-		const body = [];
-		req.on('data', (chuck) => {
-			console.log(chuck);
-			body.push(chuck);
-		});
-		req.on('end', () => {
-			const parsedBody = Buffer.concat(body).toString();
-			console.log(parsedBody);
-		});
-		res.statusCode = 302;
-		res.setHeader('Location', '/');
-		return res.end();
-	}
-
-	res.setHeader('Content-Type', 'text/html');
-	res.write('<html>');
-	res.write('<head><title>My First Page</title></head>');
-	res.write('<body><h1>Hello from my Node.js server</h1></body>');
-	res.write('</html>');
-	res.end();
-});
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
