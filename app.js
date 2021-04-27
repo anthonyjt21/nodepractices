cab9ed1b0ba8478d0d660991a0ac8cc3539a0817
ajimenez <ajimenez>
2021-04-27 08:32:21 -0600

    router admin

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
+app.use((req,res,next) =>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
 //const server = http.createServer(app);
 
 //server.listen(3000);
