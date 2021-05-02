dcd12ed563c729ae962488d534445c132a638dd7
ajimenez <anthonyjt21>
2021-05-02 13:22:36 -0600

    primera semana

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path')
+const rootDir = require('../util/path');
 
 const adminData = require('./admin');
 
@@ -12,7 +12,7 @@ router.get('/', (req, res, next) => {
 	//console.log('shop.js',adminData.products);
 	//res.sendFile(path.join(rootDir,'views','shop.html'));
 	const products = adminData.products;
-	res.render('shop',{prods:products, docTitle:'Shop'});
+	res.render('shop', {prods: products, docTitle: 'Shop Website'});
 });
 
 module.exports = router;
