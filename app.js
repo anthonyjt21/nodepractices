b807fc93e7e9c0176dd9844f7bfe1e9e3bc6efd8
ajimenez <anthonyjt21>
2021-05-18 20:21:44 -0600

    add finish pug

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const rootDir = require('./util/path');
 
 const app = express();
 
-app.set('view engine','pug');
-app.set('views','views');
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next) =>{
-   //  res.status(404).sendFile(path.join(rootDir,'views','404.html'));
-   res.status(404).render('404');
+app.use((req, res, next) => {
+	//  res.status(404).sendFile(path.join(rootDir,'views','404.html'));
+	res.status(404).render('404', {pageTitle: 'Page not Found'});
 });
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path')
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
@@ -10,17 +10,20 @@ const products = [];
 
 //admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-   res.render('add-product',{pageTitle: 'Add Product'});
-//	res.sendFile(path.join(rootDir,'views','add-product.html'));
+	res.render('add-product', {
+		pageTitle: 'Add Product',
+		path: '/admin/add-product',
+	});
+	//	res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 // admin/add-product =>POST
 router.post('/add-product', (req, res, next) => {
-//	console.log(req.body);
-products.push({title: req.body.title});
+	//	console.log(req.body);
+	products.push({title: req.body.title});
 	res.redirect('/');
 });
 
 //module.exports = router;
-exports.routes= router;
-exports.products=products;
+exports.routes = router;
+exports.products = products;
