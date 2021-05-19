7b443b648389f7b9b5ece876b972572d7591452d
ajimenez <anthonyjt21>
2021-05-18 21:04:17 -0600

    agregar handlebars

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 const rootDir = require('./util/path');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+//app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
