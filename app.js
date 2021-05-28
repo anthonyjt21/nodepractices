82a4bab89eba72b0ce0f47f9d747cb95c17f8cf9
ajimenez <ajimenez>
2021-05-28 16:08:25 -0600

    implementing mvc

--- app.js
+++ app.js
@@ -2,17 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
-const rootDir = require('./util/path');
 
+const errorController = require('./controllers/error')
 const app = express();
 
-/*app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  }));
-app.set('view engine', 'hbs');*/
+
 
 app.set('view engine','ejs');
 
@@ -20,17 +14,14 @@ app.set('view engine','ejs');
 
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	//  res.status(404).sendFile(path.join(rootDir,'views','404.html'));
-	res.status(404).render('404', {pageTitle: 'Page not Found',path: 'Error'});
-});
+app.use(errorController.get404);
 app.listen(3000);
--- util/path.js
+++ util/path.js
@@ -1,13 +1,3 @@
-var path = require('path');
+const path = require('path');
 
-// find the first module to be loaded
-var topModule = module;
-
-while(topModule.parent)
-  topModule = topModule.parent;
-
-var appDir = path.dirname(topModule.filename);
-console.log(appDir);
-var appDir2 =path.dirname(process.mainModule.filename);
-console.log(appDir2);
-module.exports=appDir
+module.exports = path.dirname(process.mainModule.filename);
